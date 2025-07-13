70441e9f558201fc2fc6d3a49bdf55c37e8fd111
Igor Cananea <icc@avalonbits.com>
2025-07-13 15:29:26 -0400

    Validate payments endpoint.

--- endpoints/api/api.go
+++ endpoints/api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/avalonbits/rinha-backend-2025/service/payment"
 	"github.com/labstack/echo/v4"
@@ -22,7 +24,25 @@ type processPaymentRequest struct {
 	Amount        float64 `json:"amount"`
 }
 
+func (r *processPaymentRequest) validate(c echo.Context) error {
+	r.CorrleationID = strings.TrimSpace(r.CorrleationID)
+	if r.CorrleationID == "" {
+		return fmt.Errorf("correlationID is required")
+	}
+
+	if r.Amount <= 0 {
+		return fmt.Errorf("amount must be positive")
+	}
+
+	return nil
+}
+
 func (h *Handler) ProcessPayment(c echo.Context) error {
+	r := processPaymentRequest{}
+	if err := h.validateRequest(c, &r); err != nil {
+		return err
+	}
+
 	return c.String(http.StatusOK, "")
 }
 
@@ -38,4 +58,19 @@ type paymentProcessor struct {
 
 func (h *Handler) PaymentSummary(c echo.Context) error {
 	return c.JSON(http.StatusOK, paymentSummaryResponse{})
+}
+
+type validator interface {
+	validate(echo.Context) error
+}
+
+func (h *Handler) validateRequest(c echo.Context, req validator) error {
+	var err error
+	if err = c.Bind(req); err == nil {
+		err = req.validate(c)
+	}
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
 }
